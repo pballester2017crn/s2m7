ded68c1bb826daa00206efc911e438e989c1a106
Pasqual Ballester Paradis <pballester@artsgrafiques.org>
2019-06-25 18:57:11 +0200

    orb

---
+++ uf23/s2m7uf23extra/orbea.js
@@ -0,0 +1,57 @@
+function k1 () {
+	document.getElementById("imatge").src = "img/T1_KATU_20.jpg";
+	}
+function k2 () {
+	document.getElementById("imatge").src = "img/T2_KATU_20.jpg";
+	}
+function k3 () {
+	document.getElementById("imatge").src = "img/T3_KATU_20.jpg";
+	}
+function k4 () {
+	document.getElementById("imatge").src = "img/T4_KATU_20.jpg";
+	}
+function k5 () {
+	document.getElementById("imatge").src = "img/T5_KATU_20.jpg";
+	}
+function k6 () {
+	document.getElementById("imatge").src = "img/T6_KATU_20.jpg";
+	}
+function k7 () {
+	document.getElementById("imatge").src = "img/T7_KATU_20.jpg";
+	}
+function k8 () {
+	document.getElementById("imatge").src = "img/T8_KATU_20.jpg";
+	}
+function Y1 () {
+	document.getElementById("imatge_carpe").src = "img/Y1_CARPE_20.jpg";
+	}
+function Y2 () {
+	document.getElementById("imatge_carpe").src = "img/Y2_CARPE_20.jpg";
+	}
+function Y3 () {
+	document.getElementById("imatge_carpe").src = "img/Y3_CARPE_20.jpg";
+	}
+function Y4 () {
+	document.getElementById("imatge_carpe").src = "img/Y4_CARPE_20.jpg";
+	}
+function YA () {
+	document.getElementById("imatge_diem").src = "img/YA_DIEM_20.jpg";
+	}
+function YB () {
+	document.getElementById("imatge_diem").src = "img/YB_DIEM_20.jpg";
+	}
+function YD () {
+	document.getElementById("imatge_diem").src = "img/YD_DIEM_20.jpg";
+	}
+function JA () {
+	document.getElementById("imatge_vector").src = "img/JA_VECTOR_20.jpg";
+	}
+function JB () {
+	document.getElementById("imatge_vector").src = "img/JB_VECTOR_20.jpg";
+	}
+function JD () {
+	document.getElementById("imatge_vector").src = "img/JD_VECTOR_20.jpg";
+	}
+function JH () {
+	document.getElementById("imatge_vector").src = "img/JH_VECTOR_20.jpg";
+	}
